d975d21482c9df38c645861461b3db31bf3f56d2
BiniTutor <70373013+binitutor@users.noreply.github.com>
2021-02-09 12:33:16 -0500

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,193 +1,408 @@
-class Booking {
-	constructor(bookDate, bookTime) {
-		this.bookDate = bookDate;
-		this.bookTime = bookTime;
-	}
-}
+demo = {
+  initPickColor: function() {
+    $('.pick-class-label').click(function() {
+      var new_class = $(this).attr('new-class');
+      var old_class = $('#display-buttons').attr('data-class');
+      var display_div = $('#display-buttons');
+      if (display_div.length) {
+        var display_buttons = display_div.find('.btn');
+        display_buttons.removeClass(old_class);
+        display_buttons.addClass(new_class);
+        display_div.attr('data-class', new_class);
+      }
+    });
+  },
 
-// user interface
-class BookingUI {
-	static createBookings() {
-		// get already created bookings from storage.
-		// in this case, local storage
-		const bookings = BookingLocalStorage.getBookingsFromStorage();
+  initDocChart: function() {
+    chartColor = "#FFFFFF";
 
-		// Api call data currently saved
-		// const bookings = ApiCall.getBookingsFromStorage();
+    ctx = document.getElementById('chartHours').getContext("2d");
 
-		// loop through the item metadata and pass to display
-		bookings.forEach( 
-			(booking) => BookingUI.displayBookings(booking) 
-		);
-	}
+    myChart = new Chart(ctx, {
+      type: 'line',
 
-	// displays booking metadata on table
-	static displayBookings(booking) {
-		// create a table row for each data passed
-		const row = document.createElement('tr');
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+        datasets: [{
+            borderColor: "#6bd098",
+            backgroundColor: "#6bd098",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 3,
+            data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
+          },
+          {
+            borderColor: "#f17e5d",
+            backgroundColor: "#f17e5d",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 3,
+            data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
+          },
+          {
+            borderColor: "#fcc468",
+            backgroundColor: "#fcc468",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 3,
+            data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
 
-		// add table data to the row
-		// it should be suitable to be in a calendar form!!
-		row.innerHTML = (`
-			<td>${booking.bookDate}</td>
-			<td>${booking.bookTime}</td>
-			<td><a class="btn btn-danger btn-sm deleteBook"> X </a></td>
-		`);
+        tooltips: {
+          enabled: false
+        },
 
-		// add row to table
-		document.querySelector('.bookList').appendChild(row);
-	}
+        scales: {
+          yAxes: [{
 
-	// this method clears the input fields after submission
-	static clearField() {
-		document.querySelector('#bookDateForm').value = '';
-  		document.querySelector('#bookTimeForm').value = '';
-	}
+            ticks: {
+              fontColor: "#9f9f9f",
+              beginAtZero: false,
+              maxTicksLimit: 5,
+              //padding: 20
+            },
+            gridLines: {
+              drawBorder: false,
+              zeroLineColor: "#ccc",
+              color: 'rgba(255,255,255,0.05)'
+            }
 
-	// this method deletes created bookings
-	static deleteBook(cell) {
-		if(cell.classList.contains('deleteBook')) {
-			cell.parentElement.parentElement.remove();
-			BookingUI.displayAlerts('Book Removed', 'danger');
-		}
-	}
+          }],
 
-	// display notifications and warnings
-	static displayAlerts(message, classField) {
-		// create a div for messages
-		const div = document.createElement('div');
+          xAxes: [{
+            barPercentage: 1.6,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(255,255,255,0.1)',
+              zeroLineColor: "transparent",
+              display: false,
+            },
+            ticks: {
+              padding: 20,
+              fontColor: "#9f9f9f"
+            }
+          }]
+        },
+      }
+    });
 
-		// add a bootstrap class name to the div
-		div.className = `alert alert-${classField}`;
+  },
 
-		// add message to the div
-		div.appendChild( document.createTextNode(message) );
+  initChartsPages: function() {
+    chartColor = "#FFFFFF";
 
-		// select the location you want this div to appear, container
-		const container = document.querySelector('.container');
+    ctx = document.getElementById('chartHours').getContext("2d");
 
-		// select the form
-		const hourTable = document.querySelector('#hourTable');
+    myChart = new Chart(ctx, {
+      type: 'line',
 
-		// display the div inside the container before the form
-		container.insertBefore(div, hourTable);
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+        datasets: [{
+            borderColor: "#6bd098",
+            backgroundColor: "#6bd098",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 3,
+            data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
+          },
+          {
+            borderColor: "#f17e5d",
+            backgroundColor: "#f17e5d",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 3,
+            data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
+          },
+          {
+            borderColor: "#fcc468",
+            backgroundColor: "#fcc468",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            borderWidth: 3,
+            data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
 
-		// make the message vanish in 3 seconds
-		setTimeout( () => document.querySelector('.alert').remove(), 3000);
-	}
-}
+        tooltips: {
+          enabled: false
+        },
 
-// this class gives access to methods of localStorage
-class BookingLocalStorage {
+        scales: {
+          yAxes: [{
 
-	// this method gets booking currently saved in local storage
-	static getBookingsFromStorage(){
-		let bookings;
-		if(localStorage.getItem('Bookings') === null) {
-			bookings = [];
-		} else {
-			bookings = JSON.parse(localStorage.getItem('Bookings'));
-		}
+            ticks: {
+              fontColor: "#9f9f9f",
+              beginAtZero: false,
+              maxTicksLimit: 5,
+              //padding: 20
+            },
+            gridLines: {
+              drawBorder: false,
+              zeroLineColor: "#ccc",
+              color: 'rgba(255,255,255,0.05)'
+            }
 
-		return bookings;
-	}
+          }],
 
-	// this method adds new booking to local storage
-	static addBookingsToStorage(newBookings){
-		// calls open bookings currently saved in local storage
-		const bookings = BookingLocalStorage.getBookingsFromStorage();
-
-		// adds newly created bookings to the list
-		bookings.push(newBookings);
-
-		// saves the updated list to local storage
-		localStorage.setItem('Bookings', JSON.stringify(bookings));
-	}
-
-	// this method removes data from local storage by matching date and time
-	static removeBookingsFromStorage(date, time){
-		// calls open bookings currently saved in local storage
-		const bookings = BookingLocalStorage.getBookingsFromStorage();
-
-		bookings.forEach( (booking, index) => {
-			if(booking.bookDate === date && booking.bookTime === time) {
-				bookings.splice(index, 1);
-			}
-		});
-
-		localStorage.setItem('Bookings', JSON.stringify(bookings));
-	}
-}
+          xAxes: [{
+            barPercentage: 1.6,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(255,255,255,0.1)',
+              zeroLineColor: "transparent",
+              display: false,
+            },
+            ticks: {
+              padding: 20,
+              fontColor: "#9f9f9f"
+            }
+          }]
+        },
+      }
+    });
 
 
-// EVENT: on page load calls createBookings method
-document.addEventListener('DOMContentLoaded', BookingUI.createBookings);
+    ctx = document.getElementById('chartEmail').getContext("2d");
 
+    myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: [1, 2, 3],
+        datasets: [{
+          label: "Emails",
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          backgroundColor: [
+            '#e3e3e3',
+            '#4acccd',
+            '#fcc468',
+            '#ef8157'
+          ],
+          borderWidth: 0,
+          data: [342, 480, 530, 120]
+        }]
+      },
 
-// EVENT: user adds new Books
-document.querySelector('#bookingCreationForm').addEventListener('submit', (e) => {
-  // Prevent actual submit
-  e.preventDefault();
+      options: {
 
-  // Get form values
-  const bookDate = document.querySelector('#bookDateForm').value;
-  const bookTime = document.querySelector('#bookTimeForm').value;
-  
-  // check if the fields are empty or not
-  if(bookDate === '' || bookTime === '')
-  {
-  	// display alert
-  	// message, className, container, beforeEl
-  	BookingUI.displayAlerts('Please fill in all fields', 'danger');
+        legend: {
+          display: false
+        },
+
+        pieceLabel: {
+          render: 'percentage',
+          fontColor: ['white'],
+          precision: 2
+        },
+
+        tooltips: {
+          enabled: false
+        },
+
+        scales: {
+          yAxes: [{
+
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              drawBorder: false,
+              zeroLineColor: "transparent",
+              color: 'rgba(255,255,255,0.05)'
+            }
+
+          }],
+
+          xAxes: [{
+            barPercentage: 1.6,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(255,255,255,0.1)',
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              display: false,
+            }
+          }]
+        },
+      }
+    });
+
+    var speedCanvas = document.getElementById("speedChart");
+
+    var dataFirst = {
+      data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
+      fill: false,
+      borderColor: '#fbc658',
+      backgroundColor: 'transparent',
+      pointBorderColor: '#fbc658',
+      pointRadius: 4,
+      pointHoverRadius: 4,
+      pointBorderWidth: 8,
+    };
+
+    var dataSecond = {
+      data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
+      fill: false,
+      borderColor: '#51CACF',
+      backgroundColor: 'transparent',
+      pointBorderColor: '#51CACF',
+      pointRadius: 4,
+      pointHoverRadius: 4,
+      pointBorderWidth: 8
+    };
+
+    var speedData = {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      datasets: [dataFirst, dataSecond]
+    };
+
+    var chartOptions = {
+      legend: {
+        display: false,
+        position: 'top'
+      }
+    };
+
+    var lineChart = new Chart(speedCanvas, {
+      type: 'line',
+      hover: false,
+      data: speedData,
+      options: chartOptions
+    });
+  },
+
+  initGoogleMaps: function() {
+    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+    var mapOptions = {
+      zoom: 13,
+      center: myLatlng,
+      scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+      styles: [{
+        "featureType": "water",
+        "stylers": [{
+          "saturation": 43
+        }, {
+          "lightness": -11
+        }, {
+          "hue": "#0088ff"
+        }]
+      }, {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "hue": "#ff0000"
+        }, {
+          "saturation": -100
+        }, {
+          "lightness": 99
+        }]
+      }, {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+          "color": "#808080"
+        }, {
+          "lightness": 54
+        }]
+      }, {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#ece2d9"
+        }]
+      }, {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#ccdca1"
+        }]
+      }, {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#767676"
+        }]
+      }, {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#ffffff"
+        }]
+      }, {
+        "featureType": "poi",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      }, {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "visibility": "on"
+        }, {
+          "color": "#b8cb93"
+        }]
+      }, {
+        "featureType": "poi.park",
+        "stylers": [{
+          "visibility": "on"
+        }]
+      }, {
+        "featureType": "poi.sports_complex",
+        "stylers": [{
+          "visibility": "on"
+        }]
+      }, {
+        "featureType": "poi.medical",
+        "stylers": [{
+          "visibility": "on"
+        }]
+      }, {
+        "featureType": "poi.business",
+        "stylers": [{
+          "visibility": "simplified"
+        }]
+      }]
+
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      title: "Hello World!"
+    });
+
+    // To add the marker to the map, call setMap();
+    marker.setMap(map);
+  },
+
+  showNotification: function(from, align) {
+    color = 'primary';
+
+    $.notify({
+      icon: "nc-icon nc-bell-55",
+      message: "Welcome to <b>Paper Dashboard</b> - a beautiful bootstrap dashboard for every web developer."
+
+    }, {
+      type: color,
+      timer: 8000,
+      placement: {
+        from: from,
+        align: align
+      }
+    });
   }
-  else {
-  	// Instatiate book object
-  	const book = new Booking(bookDate, bookTime);
 
-  	// add bookings to UI
-  	BookingUI.displayBookings(book);
-
-	// add bookings to local storage
-	BookingLocalStorage.addBookingsToStorage(book);
-
-	// display alert for successfully added book
-  	BookingUI.displayAlerts('Book Added', 'success');
-
-  	// calls the method to Clear the fields
-  	BookingUI.clearField();
-  }
-});
-
-
-
-// EVENT: delete a book from the list
-document.querySelector('#hourTable').addEventListener('click', (e) => {
-	// Remove book from UI
-	BookingUI.deleteBook(e.target);
-	
-	// date and time of selected row
-	const bookDate = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
-	const bookTime = e.target.parentElement.previousElementSibling.textContent;
-	
-	// Remove book from local storage
-	BookingLocalStorage.removeBookingsFromStorage(bookDate, bookTime);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
